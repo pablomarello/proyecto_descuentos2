5796e4c379e2012dd8c657f859f604f34f820f49
Mmelian2 <micaela.melian02@gmail.com>
2024-10-29 16:13:27 -0300

    busqueda y filtros

--- usuario/urls.py
+++ usuario/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 urlpatterns = [ 
     path('',views.index, name='index'),
+    path('buscar/', views.buscar, name='buscar'),
     #path('login/',views.login_user, name='login_user'),
     #path('logout',views.logout_user, name='logout_user'),
     path('registrar_usuario/<int:persona_id>',views.registrar_usuario, name='registrar_usuario'),
