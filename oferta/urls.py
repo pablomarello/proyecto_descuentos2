509d3499f71e511375b1a1eab9505f4fe460f3e6
Alexisquinteros121 <quinterosalexis121@gmail.com>
2024-10-31 00:22:04 -0300

    implementaciones de mapas

--- oferta/urls.py
+++ oferta/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('crear_oferta/', views.crear_oferta, name='crear_oferta'),
     path('mis_ofertas/', views.mis_ofertas, name='mis_ofertas'),
     path('buscar-productos/', views.buscar_productos, name='buscar_productos'),
-     path('recibir_puntuacion/<int:oferta_id>/', views.recibir_puntuacion,name='recibir_puntuacion'),
+    path('recibir_puntuacion/<int:oferta_id>/', views.recibir_puntuacion,name='recibir_puntuacion'),
     path('detalle_oferta/<int:oferta_id>/', views.detalle_oferta, name='detalle_oferta'),
     path('trazar_ruta/<int:oferta_id>/',views.trazar_ruta, name='trazar_ruta'),
-
+    path('comercios_cercanos/',views.comercios_cercanos, name='comercios_cercanos')
 
 
 ]
